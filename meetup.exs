ab37176159b1e0a64acb25826e43389460927508
Alexander Teno <alexander.teno@gmail.com>
2017-05-24 13:59:22 -0400

    first commit

---
+++ meetup.exs
@@ -0,0 +1,44 @@
+defmodule Meetup do
+  @moduledoc """
+  Calculate meetup dates.
+  """
+
+  @type weekday ::
+      :monday | :tuesday | :wednesday
+    | :thursday | :friday | :saturday | :sunday
+
+  @type schedule :: :first | :second | :third | :fourth | :last | :teenth
+
+  @weekday_to_index %{
+    :monday => 1,
+    :tuesday => 2,
+    :wednesday => 3,
+    :thursday => 4,
+    :friday => 5,
+    :saturday => 6,
+    :sunday => 7
+  }
+
+  @doc """
+  Calculate a meetup date.
+
+  The schedule is in which week (1..4, last or "teenth") the meetup date should
+  fall.
+  """
+  @spec meetup(pos_integer, pos_integer, weekday, schedule) :: :calendar.date
+  def meetup(year, month, weekday, schedule) do
+    cond do
+      schedule == :first -> {year, month, _get_month_day(year, month, 1..7, @weekday_to_index[weekday])}
+      schedule == :second -> {year, month, _get_month_day(year, month, 8..14, @weekday_to_index[weekday])}
+      schedule == :third -> {year, month, _get_month_day(year, month, 15..21, @weekday_to_index[weekday])}
+      schedule == :fourth -> {year, month, _get_month_day(year, month, 22..28, @weekday_to_index[weekday])}
+      schedule == :teenth -> {year, month, _get_month_day(year, month, 13..19, @weekday_to_index[weekday])}
+      # TODO: schedule == :teenth
+      true -> {:error, "Unknown schedule"}
+    end
+  end
+
+  defp _get_month_day(year, month, days_to_consider, weekday) do
+    days_to_consider |> Enum.find(fn(day_to_consider) -> Calendar.ISO.day_of_week(year, month, day_to_consider) == weekday end)
+  end
+end
